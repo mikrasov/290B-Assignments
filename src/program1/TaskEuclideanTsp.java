26fbf1a9d7a564b1374b521bd70f1b4feb9b6016
Michael Nekrasov <mikrasov@cs.ucsb.edu>
2015-04-02 15:56:28 -0700

    Add Euclidean Computation
    Compute Euclidean distance between two cities

--- src/program1/TaskEuclideanTsp.java
+++ src/program1/TaskEuclideanTsp.java
@@ -14,4 +14,14 @@ public class TaskEuclideanTsp implements Task{
 		return null;
 	}
 
+	/**
+	 * @param x1 x coordinate of city 1
+	 * @param x2 x coordinate of city 2
+	 * @param y1 y coordinate of city 1
+	 * @param y2 y coordinate of city 2
+	 * @return
+	 */
+	public static double euclidieanDistance(int x1, int x2, int y1, int y2){
+		return Math.sqrt(Math.pow((x1-x2), 2) + Math.pow( (y1-y2), 2));
+	}
 }
