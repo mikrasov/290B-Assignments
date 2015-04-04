2db2bdb9b33c7656044fd29c687bc53fb44e5f76
Michael Nekrasov <mikrasov@cs.ucsb.edu>
2015-04-03 18:23:52 -0700

    Computer Implementation
    Fill out implementation on computer obj

--- src/program1/ComputerImpl.java
+++ src/program1/ComputerImpl.java
@@ -1,21 +1,38 @@
 package program1;
 
+import java.rmi.AccessException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class ComputerImpl implements Computer{
 
-	public ComputerImpl() {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public <T> T execute(Task<T> task) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+		return task.execute();
 	}
 
+	/**
+	 * Run a new Computer on a new JVM
+	 * 
+	 * @param args				no command line arguments
+	 * @throws AccessException	no permission to bind to RMI
+	 * @throws RemoteException	problems connecting to computer
+	 */
+	public static void main(String[] args) throws AccessException, RemoteException {
+		
+		// Set Secutiry Manager 
+		System.setSecurityManager( new SecurityManager() );
 
+		// Create Registry on JVM
+        Registry registry = LocateRegistry.createRegistry( Computer.PORT );
 
+        // Create Computer
+     	ComputerImpl computer = new ComputerImpl();
+        registry.rebind( Computer.SERVICE_NAME, computer );
 
+        //Print Acknowledgement
+        System.out.println("Computer ready and registered as '"+Computer.SERVICE_NAME+"' on port "+Computer.PORT);
+	}
 
 }
