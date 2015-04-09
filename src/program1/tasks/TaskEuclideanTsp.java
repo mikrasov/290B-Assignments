4f7d8d18624c24fd8b2e0e90651b4c8d95832484
Michael Nekrasov <mikrasov@cs.ucsb.edu>
2015-04-09 15:02:10 -0700

    Update Javadoc

--- src/program1/tasks/TaskEuclideanTsp.java
+++ src/program1/tasks/TaskEuclideanTsp.java
@@ -9,7 +9,7 @@ import org.paukov.combinatorics.ICombinatoricsVector;
 import program1.api.Task;
 
 /**
- * An implementation of Task that solves the Traveling Salesman PRoblem
+ * An implementation of Task that solves the Traveling Salesman Problem
  * @author Michael, Roman
  *
  */
@@ -31,8 +31,8 @@ public class TaskEuclideanTsp implements Task<List<Integer>>{
 
 	@Override
     /**
-     * Computes traveling salesman route (not guranteed to be optimal) 
-     * @return an aproximate order of cities to visit represented by their ID
+     * Computes traveling salesman route 
+     * @return order of cities to visit represented by their ID
      */
 	public List<Integer> execute() {
 		double[][] distances = new double[cities.length][cities.length];
@@ -84,6 +84,14 @@ public class TaskEuclideanTsp implements Task<List<Integer>>{
 		return bestOrder.getVector();
 	}
 	
+	/**
+	 * Calculates the euclidian distance between two cities
+	 * @param x1 x position of city 1
+	 * @param y1 y position of city 1
+	 * @param x2 x position of city 2
+	 * @param y2 y position of city 2
+	 * @return the euclidean distance
+	 */
 	private double distance(double x1, double y1, double x2, double y2){
 		return Math.sqrt(Math.pow( (x1-x2), 2) + Math.pow( (y1-y2), 2));
 	}
