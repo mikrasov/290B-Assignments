9bcc1609a0eace1f50d104597eb6c4d4ec71b93f
Michael Nekrasov <mikrasov@cs.ucsb.edu>
2015-04-10 20:02:56 -0700

    Merge Client code from HW1

--- hw2_cli/src/client/Client.java
+++ hw2_cli/src/client/Client.java
@@ -1,14 +1,57 @@
 package client;
 
-public class Client {
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	public Client() {
-		// TODO Auto-generated constructor stub
-		
-		// Get a remote ref to server space
-		
-	}
-	
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+
+import system.SpaceImpl;
+import api.Space;
+
+public class Client<T> extends JFrame
+{
+    final protected Job job;
+    final protected Space space;
+    
+    protected T taskReturnValue;
+    private long clientStartTime;
+
+	public Client( final String title, final String domainName, final Job job ) 
+            throws RemoteException, NotBoundException, MalformedURLException
+    {     
+        this.job = job;
+        setTitle( title );
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        
+        String url = "rmi://" + domainName + ":" + Space.PORT + "/" + Space.SERVICE_NAME;
+        space = ( domainName == null ) ? new SpaceImpl() : (Space) Naming.lookup( url );
+    }
+    
+    public void begin() { clientStartTime = System.nanoTime(); }
+    
+    public void end() 
+    { 
+        Logger.getLogger( Client.class.getCanonicalName() )
+            .log(Level.INFO, "Client time: {0} ms.", ( System.nanoTime() - clientStartTime) / 1000000 );
+    }
+    
+    public void add( final JLabel jLabel )
+    {
+        final Container container = getContentPane();
+        container.setLayout( new BorderLayout() );
+        container.add( new JScrollPane( jLabel ), BorderLayout.CENTER );
+        pack();
+        setVisible( true );
+    }
+  
 	
 
 }
--- hw2_cli/src/client/ClientMandelbro.java
+++ hw2_cli/src/client/ClientMandelbro.java
@@ -1,5 +1,16 @@
 package client;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 public class ClientMandelbro extends Client {
 
 	private static final double LOWER_LEFT_X = -0.7510975859375;
@@ -8,9 +19,29 @@ public class ClientMandelbro extends Client {
     private static final int N_PIXELS = 1024;
     private static final int ITERATION_LIMIT = 512;
     
-	public ClientMandelbro() {
-		// TODO Auto-generated constructor stub
+	public ClientMandelbro(String domainName, Job job)
+			throws RemoteException, NotBoundException, MalformedURLException {
+		super("Mandelbrot Set Visualizer", domainName, job);
 	}
+    
+    public JLabel getLabel( Integer[][] counts )
+    {
+        final Image image = new BufferedImage( N_PIXELS, N_PIXELS, BufferedImage.TYPE_INT_ARGB );
+        final Graphics graphics = image.getGraphics();
+        for ( int i = 0; i < counts.length; i++ )
+            for ( int j = 0; j < counts.length; j++ )
+            {
+                graphics.setColor( getColor( counts[i][j] ) );
+                graphics.fillRect( i, N_PIXELS - j, 1, 1 );
+            }
+        final ImageIcon imageIcon = new ImageIcon( image );
+        return new JLabel( imageIcon );
+    }
+    
+    private Color getColor( int iterationCount )
+    {
+        return iterationCount == ITERATION_LIMIT ? Color.BLACK : Color.WHITE;
+    }
 
 	public static void main(String[] args) {
 		
