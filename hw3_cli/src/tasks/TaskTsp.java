dabb878a04d95902cf82bb00cf20cdba480db725
Roman Kazarin <rkazarin@umail.ucsb.edu>
2015-04-21 15:34:29 -0700

    some fixes

--- hw3_cli/src/tasks/TaskTsp.java
+++ hw3_cli/src/tasks/TaskTsp.java
@@ -1,6 +1,7 @@
 package tasks;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import util.PermutationEnumerator;
 import system.ResultTasks;
@@ -13,14 +14,14 @@ public class TaskTsp extends Closure<ChunkTsp> {
 	/** Serial ID */
 	private static final long serialVersionUID = 5656498160577890305L;
 
-	public TaskTsp(long target, int targetPort, List<Integer> fixedCities, List<Integer> toPermute, double[] cities) {
+	public TaskTsp(long target, int targetPort, List<Integer> fixedCities, List<Integer> toPermute, double[][] cities) {
 		super("TSP", target, targetPort, 3);
 		this.setInput(0, cities);
 		this.setInput(1, fixedCities);
 		this.setInput(2, toPermute);
 	}
 
-	public TaskTsp(double[] cities){
+	public TaskTsp(double[][] cities){
 		super("TSP", target, targetPort, 1);
 		this.setInput(0, cities);
 		this.setInput(1, new List<Integer>());
@@ -32,7 +33,7 @@ public class TaskTsp extends Closure<ChunkTsp> {
 	}
 
 	@Override
-	public Result<Integer> execute() {
+	public Result<ChunkTsp> execute() {
 		long clientStartTime = System.nanoTime();
 
 		List<Integer> fixedCities = (List<Integer>)input[1];
@@ -47,19 +48,19 @@ public class TaskTsp extends Closure<ChunkTsp> {
 			for(int src=0; src < cities.length; src++){
 				//Compute distance to neighbors (that are not already computed)
 				for(int dest=src+1; dest < cities.length; dest++){
-					distances[src][dest] = euclideanDistance(src, dest);
+					distances[src][dest] = euclideanDistance(src, dest, cities);
 				}
 			}
 
 			PermutationEnumerator<Integer> generator = new PermutationEnumerator(toPermute);
 
-			double bestLength = Doube.MAX_VALUE;
+			double bestLength = Double.MAX_VALUE;
 			List<Integer> bestOrder = null;
 			for(List<Integer> perm : generator){
 				double currentLength = 0;
 				perm.addAll(fixedCities);
 
-				int src = fixedCitie.get(0);
+				int src = fixedCities.get(0);
 				for(int dest: perm){
 					if(src < dest) //Compensate for triangular matrix
 						currentLength += distances[src][dest];
@@ -77,19 +78,18 @@ public class TaskTsp extends Closure<ChunkTsp> {
 				}
 			}
 
-		ResultValue<ChunkTsp> result = new Result<ChunkTsp>(new ChunkTsp(bestOrder, bestLength));
-		return result;
+		return new ResultValue<ChunkTsp>(new ChunkTsp(bestOrder, bestLength));
 
 		}
 		else {
 			@SuppressWarnings("unchecked")
-			Closure<ChunkTsp>[] tasks = new Closure<ChunkTSP>[toPermute.length+1];
-			tasks[0] = new TaskComparareTSP(target, targetPort, toPermute.length);
-			for(int i = 1; i <= toPermute.length; i++){
+			Closure<ChunkTsp>[] tasks = new Closure[toPermute.size()+1];
+			tasks[0] = new TaskCompareTsp(targetUid, targetPort, toPermute.size());
+			for(int i = 1; i <= toPermute.size(); i++){
 				//clone the fixed cities list
-				List new_fixedCities = (List) fixedCities.clone();
+				List<Integer> new_fixedCities = copyList(fixedCities);
 				//clone the toPermute list
-				List new_toPermute = (List) toPermute.clone();
+				List<Integer> new_toPermute = copyList(toPermute);
 				//add a new city to the fixed cities
 				new_fixedCities.add(new_toPermute.get(i-1));
 				//now delete that city from the new toPermute list
@@ -97,17 +97,25 @@ public class TaskTsp extends Closure<ChunkTsp> {
 				tasks[i] = new TaskTsp(-1, i-1, new_fixedCities, new_toPermute, cities);
 			}
 
-			return ResultTasks<ChunkTsp>(tasks);
+			return new ResultTasks<ChunkTsp>(tasks);
 
 		}
 	}
 
-	private double euclideanDistance(int city1, int city2){
+	private double euclideanDistance(int city1, int city2, double[][] cities){
 		double x1 = cities[city1][0];
 		double y1 = cities[city1][1];
 		double x2 = cities[city2][0];
 		double y2 = cities[city2][1];
 		return Math.sqrt(Math.pow( (x1-x2), 2) + Math.pow( (y1-y2), 2));
+	}
+
+	private List<Integer> copyList(List<Integer> list){
+		List<Integer> newList = new ArrayList<Integer>();
+		for(int i = 0; i < list.size(); i++){
+			newList.add(list.get(i));
+		}
+		return newList;
 	}
 
 }
